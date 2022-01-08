d6e091a310f65f32b34c45da22550c3d249601d6
lyf <2583701681@qq.com>
2022-01-08 11:51:17 +0800

    update test

--- Core/HAL/driver/ws2812b/ws2812b.c
+++ Core/HAL/driver/ws2812b/ws2812b.c
@@ -18,6 +18,8 @@ color_rgb blue = {0, 0, 20};
 color_rgb off = {0, 0, 0};
 
 
+
+
 //工具函数，转换rgb值到一个长度24的uint16数组（TIMER的CCR寄存器为16位）
 void rgb_2_raw(uint16_t* buff, color_rgb color) {
     for (uint16_t i = 0; i < 8; ++i) {
