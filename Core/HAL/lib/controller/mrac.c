4ce125fbab6f74e1959bfc31c4ff0f244c30edde
LMH12138 <minghangli@stu.hit.edu.cn>
2022-04-28 17:35:45 +0800

    begin mrac-controller

--- Core/HAL/lib/controller/mrac.c
+++ Core/HAL/lib/controller/mrac.c
@@ -3,9 +3,11 @@
  * @brief          :
  * @Author         : 李鸣航
  * @Date           : 2022-04-10 09:36
- * @LastEditTime   : 2022-04-28 17:29
+ * @LastEditTime   : 2022-04-28 17:34
  * @Note           :
  * @Copyright(c)   : 哈尔滨工业大学（深圳）南工骁鹰机器人队版权所有 Critical HIT copyrighted
  */
 
 #include "mrac.h"
+
+#include "math.h"
--- Core/HAL/lib/controller/mrac.h
+++ Core/HAL/lib/controller/mrac.h
@@ -1,4 +1,7 @@
 #ifndef _MRAC_H_
 #define _MRAC_H_
 
+typedef struct MRAC_t {
+} MRAC_t;
+
 #endif
