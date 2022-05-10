84e16a4f4fa3b5d3605cbfef1b11596604071b79
yzk <kgyu001@163.com>
2022-05-10 15:24:04 +0800

    add pwm readccr

--- Core/BSP/interface/bsp_pwm.h
+++ Core/BSP/interface/bsp_pwm.h
@@ -8,4 +8,5 @@ void BSP_PWM_StartCCR_DMA(uint8_t pwm_index, uint32_t* ccr_data, uint16_t len);
 void BSP_PWM_Stop(uint8_t pwm_index);
 void BSP_PWM_SetCCR(uint8_t pwm_index, uint32_t ccr_value);
 void BSP_PWM_SetARR(uint8_t pwm_index, uint32_t arr_value);
+uint32_t BSP_PWM_ReadCCR(uint8_t pwm_index);
 #endif
