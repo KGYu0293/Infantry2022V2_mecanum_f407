c7717e1b2f4538332dda6ec71801d84ad07ae77c
cxnaive <1012033254@qq.com>
2022-05-18 23:11:50 +0800

    restore common.c

--- Core/HAL/lib/common.c
+++ Core/HAL/lib/common.c
@@ -1,23 +1,11 @@
 #include <common.h>
 
 float invSqrt(float x) {
-    union {
-        float y1;
-        long y2;
-    } data;
-
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
-
-    x2 = x * 0.5F;
-    data.y1 = x;
-    i = data.y2;
+    float halfx = 0.5f * x;
+    float y = x;
+    long i = *(long *)&y;
     i = 0x5f3759df - (i >> 1);
-    data.y2 = i;
-    y = data.y1;
-    y = y * (threehalfs - (x2 * y * y));
-    y = y * (threehalfs - (x2 * y * y));
-
+    y = *(float *)&i;
+    y = y * (1.5f - (halfx * y * y));
     return y;
 }
