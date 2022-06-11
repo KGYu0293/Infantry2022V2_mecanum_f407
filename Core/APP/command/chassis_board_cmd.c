8378d1959a9eb75b7e66968a778697ed6334442f
谭蕴哲 <10304916+tan-yunzhe@user.noreply.gitee.com>
2022-06-12 07:41:14 +0800

    update ui

--- Core/APP/command/chassis_board_cmd.c
+++ Core/APP/command/chassis_board_cmd.c
@@ -164,6 +164,7 @@ void Chassis_board_CMD_Update(chassis_board_cmd* obj) {
             obj->ui->data.pc_online = obj->recv_data->pc_online;
             obj->ui->data.vision_has_taget = obj->recv_data->vision_has_target;
             obj->ui->data.power_mode = obj->recv_data->chassis_dispatch_mode;
+            obj->ui->data.bullet_speed = obj->referee->rx_data.game_robot_state.shooter_id1_17mm_speed_limit;
         }
         Robot_UI_update(obj->ui);
     }
